52713ac4eb1acbc88ed43de323fcd36524ecb0d1
Biibit1 <bianca.marinica24@campusfp.es>
2026-02-02 14:25:24 +0100

    arreglos MicroFirstTime

---
+++ My Hollow Signals/Assets/Scripts/AudioMicroScripts/MicrophoneFirstTime.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class MicrophoneFirstTime : MonoBehaviour
+{
+    public GameObject microphoneSelector;
+    public GameObject audioMeter;
+    public GameObject microphoneCalibration;
+    public GameObject noMicroText;
+
+    void Start()
+    {
+        CheckMicrophone();
+    }
+
+    void CheckMicrophone()
+    {
+        if (Microphone.devices.Length > 0)
+        {
+            // Micro detectado → mostrar opciones de micro
+            microphoneSelector.SetActive(true);
+            audioMeter.SetActive(true);
+            microphoneCalibration.SetActive(true);
+            noMicroText.SetActive(false);
+        }
+        else
+        {
+            // No hay micro → mostrar mensaje y ocultar opciones
+            microphoneSelector.SetActive(false);
+            audioMeter.SetActive(false);
+            microphoneCalibration.SetActive(false);
+            noMicroText.SetActive(true);
+        }
+    }
+}
